5221825ee39e59d259acece5742ba7d6d500f01b
bigbass1997 <bigbass1997.website@gmail.com>
2017-12-26 02:23:30 -0600

    Updated .gitignore to ignore screenshots, and enlarged the program window size.

--- desktop/src/com/bigbass/phyllotaxis/desktop/DesktopLauncher.java
+++ desktop/src/com/bigbass/phyllotaxis/desktop/DesktopLauncher.java
@@ -8,8 +8,9 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
-		config.width = 1100;
-		config.height = 800;
+		config.width = 1200;
+		config.height = 900;
+		
 		config.resizable = false;
 		
 		config.vSyncEnabled = false;
